fe7215d24bfb34024f79a67c2e4432911fc8ac92
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-03-20 13:02:39 -0300

    Add functions to print/type text centered on the screen.

--- internal/typewriter/typewriter.go
+++ internal/typewriter/typewriter.go
@@ -3,6 +3,7 @@ package typewriter
 import (
 	"os"
 	"time"
+	"unicode/utf8"
 )
 
 func Write(str string) {
@@ -14,4 +15,28 @@ func Type(str string) {
 		os.Stdout.Write([]byte{b})
 		time.Sleep(20 * time.Millisecond)
 	}
+}
+
+func WriteCenterd(str string, size int) {
+	Write(padStr(str, size))
+}
+
+func TypeCentered(str string, size int) {
+	Type(padStr(str, size))
+}
+
+func padStr(str string, size int) string {
+	var padding int
+	var paddedStr string
+	// TODO: Count COLUMNS and not characters!
+	if utf8.RuneCountInString(str) > size {
+		str = str[:size]
+	} else {
+		padding = (size - utf8.RuneCountInString(str)) / 2
+	}
+
+	for i := 0; i < padding; i++ {
+		paddedStr += " "
+	}
+	return string(paddedStr) + str
 }
