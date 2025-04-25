ebc66535193ba5baa8140811254593aa5f3b4115
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-04-25 17:46:35 -0300

    Organise kana pkg.

--- pkg/kana/hiragana.go
+++ pkg/kana/hiragana.go
@@ -5,93 +5,93 @@ package kana
 // Romaji for different kanas.
 const (
 	// 五十音（ごじゅうおん）Monographs.
-	Hg_a Hiragana = 'あ'
-	Hg_i Hiragana = 'い'
-	Hg_u Hiragana = 'う'
-	Hg_e Hiragana = 'え'
-	Hg_o Hiragana = 'お'
+	Hg_a Hiragana = "あ"
+	Hg_i Hiragana = "い"
+	Hg_u Hiragana = "う"
+	Hg_e Hiragana = "え"
+	Hg_o Hiragana = "お"
 
-	Hg_ka Hiragana = 'か'
-	Hg_ki Hiragana = 'き'
-	Hg_ku Hiragana = 'く'
-	Hg_ke Hiragana = 'け'
-	Hg_ko Hiragana = 'こ'
+	Hg_ka Hiragana = "か"
+	Hg_ki Hiragana = "き"
+	Hg_ku Hiragana = "く"
+	Hg_ke Hiragana = "け"
+	Hg_ko Hiragana = "こ"
 
-	Hg_sa Hiragana = 'さ'
-	Hg_si Hiragana = 'し'
-	Hg_su Hiragana = 'す'
-	Hg_se Hiragana = 'せ'
-	Hg_so Hiragana = 'そ'
+	Hg_sa Hiragana = "さ"
+	Hg_si Hiragana = "し"
+	Hg_su Hiragana = "す"
+	Hg_se Hiragana = "せ"
+	Hg_so Hiragana = "そ"
 
-	Hg_ta Hiragana = 'た'
-	Hg_ti Hiragana = 'ち'
-	Hg_tu Hiragana = 'つ'
-	Hg_te Hiragana = 'て'
-	Hg_to Hiragana = 'と'
+	Hg_ta Hiragana = "た"
+	Hg_ti Hiragana = "ち"
+	Hg_tu Hiragana = "つ"
+	Hg_te Hiragana = "て"
+	Hg_to Hiragana = "と"
 
-	Hg_na Hiragana = 'な'
-	Hg_ni Hiragana = 'に'
-	Hg_nu Hiragana = 'ぬ'
-	Hg_ne Hiragana = 'ね'
-	Hg_no Hiragana = 'の'
+	Hg_na Hiragana = "な"
+	Hg_ni Hiragana = "に"
+	Hg_nu Hiragana = "ぬ"
+	Hg_ne Hiragana = "ね"
+	Hg_no Hiragana = "の"
 
-	Hg_ha Hiragana = 'は'
-	Hg_hi Hiragana = 'ひ'
-	Hg_hu Hiragana = 'ふ'
-	Hg_he Hiragana = 'へ'
-	Hg_ho Hiragana = 'ほ'
+	Hg_ha Hiragana = "は"
+	Hg_hi Hiragana = "ひ"
+	Hg_hu Hiragana = "ふ"
+	Hg_he Hiragana = "へ"
+	Hg_ho Hiragana = "ほ"
 
-	Hg_ma Hiragana = 'ま'
-	Hg_mi Hiragana = 'み'
-	Hg_mu Hiragana = 'む'
-	Hg_me Hiragana = 'め'
-	Hg_mo Hiragana = 'も'
+	Hg_ma Hiragana = "ま"
+	Hg_mi Hiragana = "み"
+	Hg_mu Hiragana = "む"
+	Hg_me Hiragana = "め"
+	Hg_mo Hiragana = "も"
 
-	Hg_ya Hiragana = 'や'
-	Hg_yu Hiragana = 'ゆ'
-	Hg_yo Hiragana = 'よ'
+	Hg_ya Hiragana = "や"
+	Hg_yu Hiragana = "ゆ"
+	Hg_yo Hiragana = "よ"
 
-	Hg_ra Hiragana = 'ら'
-	Hg_ri Hiragana = 'り'
-	Hg_ru Hiragana = 'る'
-	Hg_re Hiragana = 'れ'
-	Hg_ro Hiragana = 'ろ'
+	Hg_ra Hiragana = "ら"
+	Hg_ri Hiragana = "り"
+	Hg_ru Hiragana = "る"
+	Hg_re Hiragana = "れ"
+	Hg_ro Hiragana = "ろ"
 
-	Hg_wa Hiragana = 'わ'
-	Hg_wo Hiragana = 'を'
-	Hg_n  Hiragana = 'ん'
+	Hg_wa Hiragana = "わ"
+	Hg_wo Hiragana = "を"
+	Hg_n  Hiragana = "ん"
 
 	// 濁点（だくてん）dakuten (diacritics).
-	Hg_ga Hiragana = 'が'
-	Hg_gi Hiragana = 'ぎ'
-	Hg_gu Hiragana = 'ぐ'
-	Hg_ge Hiragana = 'げ'
-	Hg_go Hiragana = 'ご'
+	Hg_ga Hiragana = "が"
+	Hg_gi Hiragana = "ぎ"
+	Hg_gu Hiragana = "ぐ"
+	Hg_ge Hiragana = "げ"
+	Hg_go Hiragana = "ご"
 
-	Hg_za Hiragana = 'ざ'
-	Hg_zi Hiragana = 'じ'
-	Hg_zu Hiragana = 'ず'
-	Hg_ze Hiragana = 'ぜ'
-	Hg_zo Hiragana = 'ぞ'
+	Hg_za Hiragana = "ざ"
+	Hg_zi Hiragana = "じ"
+	Hg_zu Hiragana = "ず"
+	Hg_ze Hiragana = "ぜ"
+	Hg_zo Hiragana = "ぞ"
 
-	Hg_da Hiragana = 'だ'
-	Hg_di Hiragana = 'ぢ'
-	Hg_du Hiragana = 'づ'
-	Hg_de Hiragana = 'で'
-	Hg_do Hiragana = 'ど'
+	Hg_da Hiragana = "だ"
+	Hg_di Hiragana = "ぢ"
+	Hg_du Hiragana = "づ"
+	Hg_de Hiragana = "で"
+	Hg_do Hiragana = "ど"
 
-	Hg_ba Hiragana = 'ば'
-	Hg_bi Hiragana = 'び'
-	Hg_bu Hiragana = 'ぶ'
-	Hg_be Hiragana = 'べ'
-	Hg_bo Hiragana = 'ぼ'
+	Hg_ba Hiragana = "ば"
+	Hg_bi Hiragana = "び"
+	Hg_bu Hiragana = "ぶ"
+	Hg_be Hiragana = "べ"
+	Hg_bo Hiragana = "ぼ"
 
 	// 半濁点　（はんだくてん）handakuten (diacritics)
-	Hg_pa Hiragana = 'ぱ'
-	Hg_pi Hiragana = 'ぴ'
-	Hg_pu Hiragana = 'ぷ'
-	Hg_pe Hiragana = 'ぺ'
-	Hg_po Hiragana = 'ぽ'
+	Hg_pa Hiragana = "ぱ"
+	Hg_pi Hiragana = "ぴ"
+	Hg_pu Hiragana = "ぷ"
+	Hg_pe Hiragana = "ぺ"
+	Hg_po Hiragana = "ぽ"
 
 	// Soma aliases for other common romaji forms.
 	// Warning: Some conflicting forms are not being included.
@@ -104,59 +104,59 @@ const (
 
 	// Digraphs.
 	// 五十音（ごじゅうおん）
-	Hg_kya Digraph = "きゃ"
-	Hg_kyu Digraph = "きゅ"
-	Hg_kyo Digraph = "きょ"
+	Hg_kya Hiragana = "きゃ"
+	Hg_kyu Hiragana = "きゅ"
+	Hg_kyo Hiragana = "きょ"
 
 	// Usually romanised as "sha, shu, sho"
-	Hg_sya Digraph = "しゃ"
-	Hg_syu Digraph = "しゅ"
-	Hg_syo Digraph = "しょ"
+	Hg_sya Hiragana = "しゃ"
+	Hg_syu Hiragana = "しゅ"
+	Hg_syo Hiragana = "しょ"
 
 	// Usually romanised as "cha, chu, cho"
-	Hg_tya Digraph = "ちゃ"
-	Hg_tyu Digraph = "ちゅ"
-	Hg_tyo Digraph = "ちょ"
+	Hg_tya Hiragana = "ちゃ"
+	Hg_tyu Hiragana = "ちゅ"
+	Hg_tyo Hiragana = "ちょ"
 
-	Hg_nya Digraph = "にゃ"
-	Hg_nyu Digraph = "にゅ"
-	Hg_nyo Digraph = "にょ"
+	Hg_nya Hiragana = "にゃ"
+	Hg_nyu Hiragana = "にゅ"
+	Hg_nyo Hiragana = "にょ"
 
-	Hg_hya Digraph = "ひゃ"
-	Hg_hyu Digraph = "ひゅ"
-	Hg_hyo Digraph = "ひょ"
+	Hg_hya Hiragana = "ひゃ"
+	Hg_hyu Hiragana = "ひゅ"
+	Hg_hyo Hiragana = "ひょ"
 
-	Hg_mya Digraph = "みゃ"
-	Hg_myu Digraph = "みゅ"
-	Hg_myo Digraph = "みょ"
+	Hg_mya Hiragana = "みゃ"
+	Hg_myu Hiragana = "みゅ"
+	Hg_myo Hiragana = "みょ"
 
-	Hg_rya Digraph = "りゃ"
-	Hg_ryu Digraph = "りゅ"
-	Hg_ryo Digraph = "りょ"
+	Hg_rya Hiragana = "りゃ"
+	Hg_ryu Hiragana = "りゅ"
+	Hg_ryo Hiragana = "りょ"
 
 	// 濁点（だくてん）dakuten (diacritics).
-	Hg_gya Digraph = "ぎゃ"
-	Hg_gyu Digraph = "ぎゅ"
-	Hg_gyo Digraph = "ぎょ"
+	Hg_gya Hiragana = "ぎゃ"
+	Hg_gyu Hiragana = "ぎゅ"
+	Hg_gyo Hiragana = "ぎょ"
 
 	// Usually romanised as "ja, ju, jo"
-	Hg_zya Digraph = "じゃ"
-	Hg_zyu Digraph = "じゅ"
-	Hg_zyo Digraph = "じょ"
+	Hg_zya Hiragana = "じゃ"
+	Hg_zyu Hiragana = "じゅ"
+	Hg_zyo Hiragana = "じょ"
 
 	// Usually also romanised as "ja, ju, jo"
-	Hg_dya Digraph = "ちゃ"
-	Hg_dyu Digraph = "ちゅ"
-	Hg_dyo Digraph = "ちょ"
+	Hg_dya Hiragana = "ちゃ"
+	Hg_dyu Hiragana = "ちゅ"
+	Hg_dyo Hiragana = "ちょ"
 
-	Hg_bya Digraph = "びゃ"
-	Hg_byu Digraph = "びゅ"
-	Hg_byo Digraph = "びょ"
+	Hg_bya Hiragana = "びゃ"
+	Hg_byu Hiragana = "びゅ"
+	Hg_byo Hiragana = "びょ"
 
 	// 半濁点　（はんだくてん）handakuten (diacritics)
-	Hg_pya Digraph = "ぴゃ"
-	Hg_pyu Digraph = "ぴゅ"
-	Hg_pyo Digraph = "ぴょ"
+	Hg_pya Hiragana = "ぴゃ"
+	Hg_pyu Hiragana = "ぴゅ"
+	Hg_pyo Hiragana = "ぴょ"
 
 	// End Hiragana.
 )
--- pkg/kana/katakana.go
+++ pkg/kana/katakana.go
@@ -5,93 +5,93 @@ package kana
 // Romaji for different kanas.
 const (
 	// 五十音（ごじゅうおん）Monographs.
-	Kk_a Katakana = 'ア'
-	Kk_i Katakana = 'イ'
-	Kk_u Katakana = 'ウ'
-	Kk_e Katakana = 'エ'
-	Kk_o Katakana = 'オ'
+	Kk_a Katakana = "ア"
+	Kk_i Katakana = "イ"
+	Kk_u Katakana = "ウ"
+	Kk_e Katakana = "エ"
+	Kk_o Katakana = "オ"
 
-	Kk_ka Katakana = 'カ'
-	Kk_ki Katakana = 'キ'
-	Kk_ku Katakana = 'ク'
-	Kk_ke Katakana = 'ケ'
-	Kk_ko Katakana = 'コ'
+	Kk_ka Katakana = "カ"
+	Kk_ki Katakana = "キ"
+	Kk_ku Katakana = "ク"
+	Kk_ke Katakana = "ケ"
+	Kk_ko Katakana = "コ"
 
-	Kk_sa Katakana = 'サ'
-	Kk_si Katakana = 'シ'
-	Kk_su Katakana = 'ス'
-	Kk_se Katakana = 'セ'
-	Kk_so Katakana = 'ソ'
+	Kk_sa Katakana = "サ"
+	Kk_si Katakana = "シ"
+	Kk_su Katakana = "ス"
+	Kk_se Katakana = "セ"
+	Kk_so Katakana = "ソ"
 
-	Kk_ta Katakana = 'タ'
-	Kk_ti Katakana = 'チ'
-	Kk_tu Katakana = 'ツ'
-	Kk_te Katakana = 'テ'
-	Kk_to Katakana = 'ト'
+	Kk_ta Katakana = "タ"
+	Kk_ti Katakana = "チ"
+	Kk_tu Katakana = "ツ"
+	Kk_te Katakana = "テ"
+	Kk_to Katakana = "ト"
 
-	Kk_na Katakana = 'ナ'
-	Kk_ni Katakana = 'ニ'
-	Kk_nu Katakana = 'ヌ'
-	Kk_ne Katakana = 'ネ'
-	Kk_no Katakana = 'ノ'
+	Kk_na Katakana = "ナ"
+	Kk_ni Katakana = "ニ"
+	Kk_nu Katakana = "ヌ"
+	Kk_ne Katakana = "ネ"
+	Kk_no Katakana = "ノ"
 
-	Kk_ha Katakana = 'ハ'
-	Kk_hi Katakana = 'ヒ'
-	Kk_hu Katakana = 'フ'
-	Kk_he Katakana = 'ヘ'
-	Kk_ho Katakana = 'ホ'
+	Kk_ha Katakana = "ハ"
+	Kk_hi Katakana = "ヒ"
+	Kk_hu Katakana = "フ"
+	Kk_he Katakana = "ヘ"
+	Kk_ho Katakana = "ホ"
 
-	Kk_ma Katakana = 'マ'
-	Kk_mi Katakana = 'ミ'
-	Kk_mu Katakana = 'ム'
-	Kk_me Katakana = 'メ'
-	Kk_mo Katakana = 'モ'
+	Kk_ma Katakana = "マ"
+	Kk_mi Katakana = "ミ"
+	Kk_mu Katakana = "ム"
+	Kk_me Katakana = "メ"
+	Kk_mo Katakana = "モ"
 
-	Kk_ya Katakana = 'ヤ'
-	Kk_yu Katakana = 'ユ'
-	Kk_yo Katakana = 'ヨ'
+	Kk_ya Katakana = "ヤ"
+	Kk_yu Katakana = "ユ"
+	Kk_yo Katakana = "ヨ"
 
-	Kk_ra Katakana = 'ラ'
-	Kk_ri Katakana = 'リ'
-	Kk_ru Katakana = 'ル'
-	Kk_re Katakana = 'レ'
-	Kk_ro Katakana = 'ロ'
+	Kk_ra Katakana = "ラ"
+	Kk_ri Katakana = "リ"
+	Kk_ru Katakana = "ル"
+	Kk_re Katakana = "レ"
+	Kk_ro Katakana = "ロ"
 
-	Kk_wa Katakana = 'ワ'
-	Kk_wo Katakana = 'ヲ'
-	Kk_n  Katakana = 'ン'
+	Kk_wa Katakana = "ワ"
+	Kk_wo Katakana = "ヲ"
+	Kk_n  Katakana = "ン"
 
 	// 濁点（だくてん）dakuten (diacritics).
-	Kk_ga Katakana = 'ガ'
-	Kk_gi Katakana = 'ギ'
-	Kk_gu Katakana = 'グ'
-	Kk_ge Katakana = 'ゲ'
-	Kk_go Katakana = 'ゴ'
+	Kk_ga Katakana = "ガ"
+	Kk_gi Katakana = "ギ"
+	Kk_gu Katakana = "グ"
+	Kk_ge Katakana = "ゲ"
+	Kk_go Katakana = "ゴ"
 
-	Kk_za Katakana = 'ザ'
-	Kk_zi Katakana = 'ジ'
-	Kk_zu Katakana = 'ズ'
-	Kk_ze Katakana = 'ゼ'
-	Kk_zo Katakana = 'ゾ'
+	Kk_za Katakana = "ザ"
+	Kk_zi Katakana = "ジ"
+	Kk_zu Katakana = "ズ"
+	Kk_ze Katakana = "ゼ"
+	Kk_zo Katakana = "ゾ"
 
-	Kk_da Katakana = 'ダ'
-	Kk_di Katakana = 'ヂ'
-	Kk_du Katakana = 'ヅ'
-	Kk_de Katakana = 'デ'
-	Kk_do Katakana = 'ド'
+	Kk_da Katakana = "ダ"
+	Kk_di Katakana = "ヂ"
+	Kk_du Katakana = "ヅ"
+	Kk_de Katakana = "デ"
+	Kk_do Katakana = "ド"
 
-	Kk_ba Katakana = 'バ'
-	Kk_bi Katakana = 'ビ'
-	Kk_bu Katakana = 'ブ'
-	Kk_be Katakana = 'ベ'
-	Kk_bo Katakana = 'ボ'
+	Kk_ba Katakana = "バ"
+	Kk_bi Katakana = "ビ"
+	Kk_bu Katakana = "ブ"
+	Kk_be Katakana = "ベ"
+	Kk_bo Katakana = "ボ"
 
 	// 半濁点　（はんだくてん）handakuten (diacritics)
-	Kk_pa Katakana = 'パ'
-	Kk_pi Katakana = 'ピ'
-	Kk_pu Katakana = 'プ'
-	Kk_pe Katakana = 'ペ'
-	Kk_po Katakana = 'ポ'
+	Kk_pa Katakana = "パ"
+	Kk_pi Katakana = "ピ"
+	Kk_pu Katakana = "プ"
+	Kk_pe Katakana = "ペ"
+	Kk_po Katakana = "ポ"
 
 	// Soma aliases for other common romaji forms.
 	// Warning: Some conflicting forms are not being included.
@@ -104,57 +104,57 @@ const (
 
 	// Digraphs.
 	// 五十音（ごじゅうおん）
-	Kk_kya Digraph = "キャ"
-	Kk_kyu Digraph = "キュ"
-	Kk_kyo Digraph = "キョ"
+	Kk_kya Katakana = "キャ"
+	Kk_kyu Katakana = "キュ"
+	Kk_kyo Katakana = "キョ"
 
 	// Usually romanised as "sha, shu, sho"
-	Kk_sya Digraph = "シャ"
-	Kk_syu Digraph = "シュ"
-	Kk_syo Digraph = "ショ"
+	Kk_sya Katakana = "シャ"
+	Kk_syu Katakana = "シュ"
+	Kk_syo Katakana = "ショ"
 
 	// Usually romanised as "cha, chu, cho"
-	Kk_tya Digraph = "チャ"
-	Kk_tyu Digraph = "チュ"
-	Kk_tyo Digraph = "チョ"
+	Kk_tya Katakana = "チャ"
+	Kk_tyu Katakana = "チュ"
+	Kk_tyo Katakana = "チョ"
 
-	Kk_nya Digraph = "ニャ"
-	Kk_nyu Digraph = "ニュ"
-	Kk_nyo Digraph = "ニョ"
+	Kk_nya Katakana = "ニャ"
+	Kk_nyu Katakana = "ニュ"
+	Kk_nyo Katakana = "ニョ"
 
-	Kk_hya Digraph = "ヒャ"
-	Kk_hyu Digraph = "ヒュ"
-	Kk_hyo Digraph = "ヒョ"
+	Kk_hya Katakana = "ヒャ"
+	Kk_hyu Katakana = "ヒュ"
+	Kk_hyo Katakana = "ヒョ"
 
-	Kk_mya Digraph = "ミャ"
-	Kk_myu Digraph = "ミュ"
-	Kk_myo Digraph = "ミョ"
+	Kk_mya Katakana = "ミャ"
+	Kk_myu Katakana = "ミュ"
+	Kk_myo Katakana = "ミョ"
 
-	Kk_rya Digraph = "リャ"
-	Kk_ryu Digraph = "リュ"
-	Kk_ryo Digraph = "リョ"
+	Kk_rya Katakana = "リャ"
+	Kk_ryu Katakana = "リュ"
+	Kk_ryo Katakana = "リョ"
 
 	// 濁点（だくてん）dakuten (diacritics).
-	Kk_gya Digraph = "ギャ"
-	Kk_gyu Digraph = "ギュ"
-	Kk_gyo Digraph = "ギョ"
+	Kk_gya Katakana = "ギャ"
+	Kk_gyu Katakana = "ギュ"
+	Kk_gyo Katakana = "ギョ"
 
 	// Usually romanised as "ja, ju, jo"
-	Kk_zya Digraph = "ジャ"
-	Kk_zyu Digraph = "シュ"
-	Kk_zyo Digraph = "ショ"
+	Kk_zya Katakana = "ジャ"
+	Kk_zyu Katakana = "シュ"
+	Kk_zyo Katakana = "ショ"
 
 	// Usually also romanised as "ja, ju, jo"
-	Kk_dya Digraph = "ヂャ"
-	Kk_dyu Digraph = "ヂュ"
-	Kk_dyo Digraph = "ヂョ"
+	Kk_dya Katakana = "ヂャ"
+	Kk_dyu Katakana = "ヂュ"
+	Kk_dyo Katakana = "ヂョ"
 
-	Kk_bya Digraph = "ビャ"
-	Kk_byu Digraph = "ビュ"
-	Kk_byo Digraph = "ビョ"
+	Kk_bya Katakana = "ビャ"
+	Kk_byu Katakana = "ビュ"
+	Kk_byo Katakana = "ビョ"
 
 	// 半濁点　（はんだくてん）handakuten (diacritics)
-	Kk_pya Digraph = "ピャ"
-	Kk_pyu Digraph = "ピュ"
-	Kk_pyo Digraph = "ピョ"
+	Kk_pya Katakana = "ピャ"
+	Kk_pyu Katakana = "ピュ"
+	Kk_pyo Katakana = "ピョ"
 )
